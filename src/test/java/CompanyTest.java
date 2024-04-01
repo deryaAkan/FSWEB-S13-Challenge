06a060354aff7337d55daa585a373369178c7d29
Derya Akan <d.akan369@gmail.com>
2024-04-01 14:04:05 +0300

    typos fixed #2

--- src/test/java/CompanyTest.java
+++ src/test/java/CompanyTest.java
@@ -86,7 +86,7 @@ public class CompanyTest {
     public void testEmployeeInstanceTypes() throws NoSuchFieldException {
         assertThat(employee.getFullName(), instanceOf(String.class));
         assertThat(employee.getEmail(), instanceOf(String.class));
-        assertThat(employee.getHealthplans(), instanceOf(String[].class));
+        assertThat(employee.getHealthPlans(), instanceOf(String[].class));
     }
 
     @DisplayName("addHealthplan method başarılı çalışıyor mu?")
